baf4e99f400d71c29e7354a00af2b35a37c80f05
janverchen <janver@foxmail.com>
2017-12-13 10:24:09 +0800

    ffff

---
+++ src/index.js
@@ -0,0 +1,14 @@
+
+var tesla = require("./createTesla.js");
+
+module.exports = function register(homebridge) {
+  const Service = homebridge.hap.Service
+  const Characteristic = homebridge.hap.Characteristic
+  homebridge.registerAccessory('homebridge-tesla-janver', 'Tesla-janver',tesla.createTesla({
+    Service,
+    Characteristic,
+  }));
+}
+
+
+
